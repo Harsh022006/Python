0e1720f9e8382ffcc5d1fdedae383c1898833e22
Harsh022006 <harshamburkar@gmail.com>
2025-09-29 23:53:05 +0530

    practice

---
+++ finding-name-dict.py
@@ -0,0 +1,12 @@
+d={1:"harsh",2:"Khushal",3:"Harshad",4:"Kartik",5:"Friend"}
+
+name=input("Enter Student Name : ")
+
+for i in d:
+    if name==d[i]:
+        print("Present")
+        break
+    
+else:
+    print("Absent")
+    
