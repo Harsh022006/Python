7fb52629f958f89ccfa7f4aaae0a496c5c9660d7
Harsh022006 <harshamburkar@gmail.com>
2025-10-07 21:30:43 +0530

    practice

---
+++ variable,comments_and_indentation.py
@@ -0,0 +1,17 @@
+#take a num using number variable
+
+num=int(input("enter Number : ")) #integer Var
+
+#use Condition Statement
+if num > 10:
+    #indented block for positive number
+    print("The Number is Positive")
+
+elif num < 10:
+    #indented block for negative number
+    print("The Number is Negative")
+
+else:
+    #indented block for zero
+    print("the Number is Zero")
+    
