5568cf44994ab9806d6c9ad178b87dce8b84a064
Harsh022006 <harshamburkar@gmail.com>
2025-10-10 20:57:51 +0530

    practice

---
+++ greater_less.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num > 10:
+    print(num, "is greater than 10")
+elif num < 10:
+    print(num, "is less than 10")
+else:
+    print(num, "is equal to 10")
