996f9ec51c887c32d2645767ac773c0ddd3d9401
Harsh022006 <harshamburkar@gmail.com>
2025-10-18 22:57:58 +0530

    practice

--- Filter_Out_Even_Numbers.py
+++
@@ -1,7 +0,0 @@
-def is_odd(x):
-    return x % 2 != 0
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-odd_numbers = list(filter(is_odd, numbers))
-
-print("List after filtering even numbers:", odd_numbers)
